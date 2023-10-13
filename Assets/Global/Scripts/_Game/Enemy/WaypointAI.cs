c96d2fa59a1da0293409aa02721dda90a73a1d31
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2023-10-13 00:16:29 -0600

    Basic Enemy AI
    it's really basic, just goes from waypoint to waypoint.

---
+++ Assets/Global/Scripts/_Game/Enemy/WaypointAI.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class WaypointAI : MonoBehaviour {
+    [SerializeField] float Speed;
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriterenderer;
+
+    [Header("AI")]
+    [SerializeField] Transform[] Waypoints;
+
+    Rigidbody2D RB;
+    Vector2 Velocity = Vector2.zero;
+    bool FaceRight = true;
+    Transform TargetWaypoint = null;
+    float Timer = .01f;
+
+    void Start() {
+        RB = GetComponent<Rigidbody2D>();
+        spriterenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        Vector2 Direction = Vector2.zero;
+        if (TargetWaypoint == null) SetNewWaypointTarget();
+
+        Direction.x = Mathf.Sign(TargetWaypoint.position.x - transform.position.x);
+        float DX = Mathf.Abs(TargetWaypoint.position.x - transform.position.x);
+        if (DX <= 0.25f) {
+            Timer -= Time.deltaTime;
+            if (Timer <= 0) SetNewWaypointTarget();
+        }
+
+        Velocity.x = Direction.x * Speed;
+
+        // move character
+        RB.velocity = Velocity;
+
+        // Rotate character to face direction of movement
+        if (Velocity.x > 0 && !FaceRight) Flip();
+        if (Velocity.x < 0 && FaceRight) Flip();
+    }
+
+    private void Flip() {
+        Vector3 CurrentScale = gameObject.transform.localScale;
+        CurrentScale.x *= -1;
+        gameObject.transform.localScale = CurrentScale;
+
+        FaceRight = !FaceRight;
+    }
+
+    private void SetNewWaypointTarget() {
+        Transform Waypoint = null;
+        do {
+            Waypoint = Waypoints[Random.Range(0, Waypoints.Length)];
+        } while (Waypoint == TargetWaypoint);
+        TargetWaypoint = Waypoint;
+    }
+}
